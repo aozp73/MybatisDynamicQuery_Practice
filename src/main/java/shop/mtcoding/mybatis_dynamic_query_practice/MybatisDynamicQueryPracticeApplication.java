05750af21c202472bd0b43c4477db77f8cafefed
aozp73 <aozp73@naver.com>
2023-02-17 12:14:09 +0900

    initial commit

---
+++ src/main/java/shop/mtcoding/mybatis_dynamic_query_practice/MybatisDynamicQueryPracticeApplication.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.mybatis_dynamic_query_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisDynamicQueryPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisDynamicQueryPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/mtcoding/mybatis_dynamic_query_practice/MybatisDynamicQueryPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.mybatis_dynamic_query_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisDynamicQueryPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
